0682ec004845c065e180e0f01f13a2eb2d9cdbea
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-01-12 15:43:15 +0100

    added event listener button generate

--- js/script.js
+++ js/script.js
@@ -4,16 +4,26 @@ console.log('js ok');
 Il programma dovrà mostrare una form da compilare con cui 
 chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, 
+secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 
-Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, 
-    per indicare centesimi sul prezzo).
+Il recap dei dati e l'output del prezzo finale va stampato in pagina 
+(formattato con massimo due decimali, per indicare centesimi sul prezzo).
 */
 
-//creo variabili degli elementi in pagina
+//recupero elementi
+const nameElement = document.getElementById('user-name');
+const distanceElement = document.getElementById('distance');
+const agesElement = document.getElementById('ages-menu');
+const generateElement = document.getElementById('generate');
+const resetElement = document.getElementById('reset');
 
 
-
+generateElement.addEventListener('click', function(){
+    console.log('click');
+    const nameValue = nameElement.value;
+    console.log(nameValue);
+})
